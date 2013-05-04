6997a03c45fb2f22347033cc91c3a0d7fced8c88
tugberkugurlu <tugberkugurlu@gmail.com>
2013-05-04 23:35:55 +0300

    added HelloWorldOwin

---
+++ HelloWorldOwin/HelloWorldOwin/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldOwin {
+    
+    public partial class Startup {
+
+        public void Configuration(IAppBuilder app) {
+
+            app.UseHandler(async (request, response) => {
+
+                response.ContentType = "text/html";
+                await response.WriteAsync("OWIN Hello World!!");
+            });
+        }
+    }
+}
