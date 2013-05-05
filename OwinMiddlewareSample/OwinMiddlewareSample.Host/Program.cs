11b9e9fc46851cd0cb7508c908217fbf50a8366b
tugberkugurlu <tugberkugurlu@gmail.com>
2013-05-05 13:30:39 +0300

    added OwinMiddlewareSample proj

---
+++ OwinMiddlewareSample/OwinMiddlewareSample.Host/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinMiddlewareSample.Host {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            using (IDisposable server = WebApp.Start<Startup>()) {
+
+                Console.WriteLine("Started...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ OwinMiddlewareSample/OwinMiddlewareSample/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Owin;
+using Owin.Types;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AppFunc = System.Func
+                <
+                    System.Collections.Generic.IDictionary<string, object>,
+                    System.Threading.Tasks.Task
+                >;
+
+namespace OwinMiddlewareSample {
+    
+    public partial class Startup {
+
+        public void Configuration(IAppBuilder app) {
+
+            app.UseType<MyMiddleware>();
+        }
+
+        public class MyMiddleware {
+
+            // This will called once when the app starts up.
+            // This takes a parameter for the next pointer.
+            // These are put together kind of like a link list
+
+            private readonly AppFunc _nextFunc;
+
+            public MyMiddleware(AppFunc nextFunc) {
+                _nextFunc = nextFunc;
+            }
+
+            public async Task Invoke(IDictionary<string, object> env) {
+
+                OwinRequest request = new OwinRequest(env);
+                if (request.Path == "/raw") {
+
+                    OwinResponse response = new OwinResponse(request);
+                    response.ContentType = "text/plain";
+                    await response.WriteAsync("Hello from OWIN!");
+                }
+                else {
+
+                    await _nextFunc(env);
+                }
+            }
+        }
+    }
+}
