de4edaf794e1d93635a947bdd3a114c055ee683d
tugberkugurlu <tugberkugurlu@gmail.com>
2013-08-01 11:15:15 +0300

    added GetStartedSamples

---
+++ GetStartedSamples/HelloWorld1.ConsoleHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld1.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>())
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stoping...");
+            }
+        }
+    }
+}
---
+++ GetStartedSamples/HelloWorld1/Startup.cs
@@ -0,0 +1,78 @@
+﻿using HelloWorld1;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppFunc = System.Func
+                <
+                    System.Collections.Generic.IDictionary<string, object>,
+                    System.Threading.Tasks.Task
+                >;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace HelloWorld1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use(new Func<AppFunc, AppFunc>(nextApp => new TimerMiddleware(nextApp).Invoke));
+            app.Use(new Func<AppFunc, AppFunc>(nextApp => new MyMiddleware(nextApp).Invoke));
+        }
+    }
+
+    public class TimerMiddleware
+    {
+        private readonly AppFunc _nextFunc;
+
+        public TimerMiddleware(AppFunc nextFunc)
+        {
+            _nextFunc = nextFunc;
+        }
+
+        public async Task Invoke(IDictionary<string, object> env)
+        {
+            Console.WriteLine("Request: " + env["owin.RequestPath"].ToString());
+            await _nextFunc(env);
+            Console.WriteLine("Response: " + env["owin.ResponseStatusCode"].ToString());
+        }
+    }
+
+    public class MyMiddleware
+    {
+        private readonly AppFunc _nextFunc;
+
+        public MyMiddleware(AppFunc nextFunc)
+        {
+            _nextFunc = nextFunc;
+        }
+
+        public async Task Invoke(IDictionary<string, object> env)
+        {
+            string path = env["owin.RequestPath"] as string;
+            if (path == "/raw")
+            {
+                Stream responseStream = (Stream)env["owin.ResponseBody"];
+                IDictionary<string, string[]> headers = (IDictionary<string, string[]>)env["owin.ResponseHeaders"];
+
+                string bodyText = string.Format("Serviced request on {0} at {1}", DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString());
+                byte[] responseBytes = ASCIIEncoding.UTF8.GetBytes(bodyText);
+
+                headers["Content-Length"] = new string[] { responseBytes.Length.ToString(CultureInfo.InvariantCulture) };
+                headers["Content-Type"] = new[] { "text/plain" };
+
+                await responseStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+            }
+            else
+            {
+                await _nextFunc(env);
+            }
+        }
+    }
+}
