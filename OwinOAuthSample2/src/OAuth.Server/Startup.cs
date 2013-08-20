ca9f7b6c087b03eb5bf7930df836d5dab80542b7
tugberkugurlu <tugberkugurlu@gmail.com>
2013-08-20 18:03:16 +0300

    added FederationMetadata

--- OwinOAuthSample2/src/OAuth.Server/Startup.cs
+++ OwinOAuthSample2/src/OAuth.Server/Startup.cs
@@ -2,18 +2,43 @@
 using Microsoft.Owin.Security.Infrastructure;
 using Microsoft.Owin.Security.Jwt;
 using Microsoft.Owin.Security.OAuth;
+using OAuth.Server;
 using Owin;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Metadata;
+using System.IdentityModel.Protocols.WSTrust;
 using System.IdentityModel.Tokens;
+using System.IO;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
+[assembly: OwinStartup(typeof(Startup))]
 namespace OAuth.Server
 {
     public class Startup
     {
+        private const string OAuthTokenEndpoint = "https://localhost:5000/oauth/token";
+        private const string FederationMetadataUrl = "/FederationMetadata/2007-06/FederationMetadata.xml";
+
         public void Configuration(IAppBuilder app)
         {
+            app.Use(async (ctx, next) =>
+            {
+                if (ctx.Request.Path == new PathString(FederationMetadataUrl))
+                {
+                    string message = Generate();
+                    ctx.Response.Headers.Add("Content-Type", new[] { "text/xml" });
+                    await ctx.Response.WriteAsync(message);
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
             app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions 
             {
                 AuthorizeEndpointPath = new PathString("/oauth/authorize"),
@@ -57,6 +82,46 @@ namespace OAuth.Server
 
         // Helpers
 
+        private string Generate()
+        {
+            var tokenServiceDescriptor = GetTokenServiceDescriptor();
+            var id = new EntityId("https://tugberk.me");
+            var entity = new EntityDescriptor(id);
+            var certificate = GetSigningCertificate();
+            entity.SigningCredentials = new X509SigningCredentials(certificate);
+            entity.RoleDescriptors.Add(tokenServiceDescriptor);
+
+            var ser = new MetadataSerializer();
+            var sb = new StringBuilder(512);
+
+            using (var sr = new StringWriter(sb))
+            using (var xmlWriter = XmlWriter.Create(sr, new XmlWriterSettings { OmitXmlDeclaration = true }))
+            {
+                ser.WriteMetadata(xmlWriter, entity);
+                return sb.ToString();
+            }
+        }
+
+        private SecurityTokenServiceDescriptor GetTokenServiceDescriptor()
+        {
+            var tokenService = new SecurityTokenServiceDescriptor();
+            tokenService.TokenTypesOffered.Add(new Uri(TokenTypes.JsonWebToken));
+            tokenService.ProtocolsSupported.Add(new Uri("http://docs.oasis-open.org/wsfed/federation/200706"));
+            tokenService.SecurityTokenServiceEndpoints.Add(new EndpointReference(OAuthTokenEndpoint));
+
+            return tokenService;
+        }
+
+        private KeyDescriptor GetSigningKeyDescriptor()
+        {
+            var certificate = GetSigningCertificate();
+            var clause = new X509SecurityToken(certificate).CreateKeyIdentifierClause<X509RawDataKeyIdentifierClause>();
+            var key = new KeyDescriptor(new SecurityKeyIdentifier(clause));
+            key.Use = KeyType.Signing;
+
+            return key;
+        }
+
         private static X509Certificate2 GetSigningCertificate()
         {
             X509Store store = new X509Store("My");
@@ -76,12 +141,17 @@ namespace OAuth.Server
 
         public SigningCredentials SigningCredentials { get { return _signingCredentials; } }
         public string Issuer { get { return _issuer; } }
-        public IEnumerable<SecurityToken> SecurityTokens { get { return null; } }
+        
 
         public X509CertificateSigningCredentialsProvider(string issuer, X509Certificate2 signingCert)
         {
             _signingCredentials = new X509SigningCredentials(signingCert);
             _issuer = issuer;
+        }
+
+        public IEnumerable<SecurityToken> SecurityTokens
+        {
+            get { throw new NotSupportedException(); }
         }
     }
 }
---
+++ OwinOAuthSample2/src/OAuth.Server/TokenTypes.cs
@@ -0,0 +1,8 @@
+﻿
+namespace OAuth.Server
+{
+    public static class TokenTypes
+    {
+        public const string JsonWebToken = "urn:ietf:params:oauth:token-type:jwt";
+    }
+}
