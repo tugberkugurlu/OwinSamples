25fcba07d4dbe3b1261244e902b1e10daac1bd4e
tugberkugurlu <tugberkugurlu@gmail.com>
2013-09-09 10:59:01 +0300

    added the OwinHost VS Integration Sample

---
+++ OwinHostVsIntegrationSample/OwinHostVsIntegrationSample/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Owin;
+using OwinHostVsIntegrationSample;
+using System.IO;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace OwinHostVsIntegrationSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            app.Use(async (ctx, next) => 
+            {
+                TextWriter output = ctx.Get<TextWriter>("host.TraceOutput");
+                output.WriteLine("{0} {1}: {2}", 
+                    ctx.Request.Scheme, ctx.Request.Method, ctx.Request.Path);
+
+                await ctx.Response.WriteAsync("Hello world!");
+            });
+        }
+    }
+}
