c7e37f27152eec1690ebbdc8a82fb3564de14892
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-09-26 17:29:23 +0300

    added diagnostics sample

---
+++ OwinDiagnosticsSample/OwinDiagnosticsSample/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Diagnostics;
+
+[assembly: OwinStartup(typeof(OwinDiagnosticsSample.Startup))]
+namespace OwinDiagnosticsSample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+            app.UseErrorPage();
+            app.Use((ctx, next) => 
+            {
+                int left = 1,
+                    right = 0,
+                    total = left / right;
+
+                return next.Invoke();
+            });
+        }
+    }
+}
