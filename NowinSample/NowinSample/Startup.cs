170e0b280c201f9ae3f304a53300052362403267
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-09-27 17:08:57 +0300

    added the Nowin sample

---
+++ NowinSample/NowinSample/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NowinSample
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use(new Func<AppFunc, AppFunc>(ignoreNext => Invoke));
+        }
+
+        public async Task Invoke(IDictionary<string, object> env)
+        {
+            // retrieve the Request Data from the environment
+            string path = env["owin.RequestPath"] as string;
+
+            if (path.Equals("/", StringComparison.OrdinalIgnoreCase))
+            {
+                // Prepare the message
+                const string Message = "Hello World!";
+                byte[] bytes = Encoding.UTF8.GetBytes(Message);
+
+                // retrieve the Response Data from the environment
+                Stream responseBody = env["owin.ResponseBody"] as Stream;
+                IDictionary<string, string[]> responseHeaders = env["owin.ResponseHeaders"] as IDictionary<string, string[]>;
+
+                // write the headers, response body
+                responseHeaders["Content-Type"] = new[] { "text/plain" };
+                await responseBody.WriteAsync(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
